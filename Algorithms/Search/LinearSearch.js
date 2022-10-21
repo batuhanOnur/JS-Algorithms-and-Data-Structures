40c429211bcbba0fb1c7fb1abd57fc6ea8de6517
batuhanOnur <batuhanonur29@gmail.com>
2022-10-21 16:29:07 +0300

    BinarySearch

--- Algorithms/Search/LinearSearch.js
+++ Algorithms/Search/LinearSearch.js
@@ -1,5 +1,5 @@
 function linearSearch(arr,target){
-    
+
    for(let i = 0; i < arr.length; i++){
       if(arr[i] === target){
         return i;
@@ -8,8 +8,10 @@ function linearSearch(arr,target){
 
    return -1;
 }
-
-
+ 
+ 
 console.log(linearSearch([-5,2,10,4,6], 10))  // 2 
 console.log(linearSearch([-5,2,10,4,6], 6))   // 4
 console.log(linearSearch([-5,2,10,4,6], 20))  // -1  
+
+// Big O = O(n)
